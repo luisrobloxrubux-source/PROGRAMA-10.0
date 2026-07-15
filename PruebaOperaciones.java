c111d06ede2443fc9f860255c1e46a56857474cc
luisrobloxrubux-source <luisrobloxrubux@gmail.com>
2026-07-15 17:59:36 -0500

    Add files via upload

--- PruebaOperaciones.java
+++ PruebaOperaciones.java
@@ -1,8 +1,14 @@
 
+
 class Operaciones {
 
-    public void imprimirTexto(String texto) {
-        System.out.println(texto);
+    public void concatenarTexto(String texto1, String texto2) {
+        System.out.println(texto1 + " " + texto2);
+    }
+
+    public void imprimirSuma(int entero1, int entero2) {
+        int suma = entero1 + entero2;
+        System.out.println("Suma: " + suma);
     }
 }
 
@@ -12,9 +18,10 @@ public class PruebaOperaciones {
 
         Operaciones operaciones = new Operaciones();
 
-        operaciones.imprimirTexto("Primer Texto");
-        operaciones.imprimirTexto("Segundo Texto");
-        operaciones.imprimirTexto("Tercer Texto");
+        operaciones.concatenarTexto("Primero", "Segundo");
+        operaciones.concatenarTexto("Tercero", "Cuarto");
 
+        operaciones.imprimirSuma(10, 20);
+        operaciones.imprimirSuma(30, 40);
     }
 }
