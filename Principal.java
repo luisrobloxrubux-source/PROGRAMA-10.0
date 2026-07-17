2d9d84fe16666d4b7b034a13ec4a567bb9291c8f
luisrobloxrubux-source <luisrobloxrubux@gmail.com>
2026-07-17 17:51:39 -0500

    Add files via upload

--- Principal.java
+++ Principal.java
@@ -2,32 +2,62 @@
 
 class Persona {
 
-    public int edad;
+    private String nombre;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+
+    public Persona() {
+        nombre = "";
+        apellidoPaterno = "";
+        apellidoMaterno = "";
+    } // Fin del constructor
+
+    public String getNombre() {
+        return nombre;
+    } // Fin del método getNombre
+
+    public void setNombre(String pNombre) {
+        nombre = pNombre;
+    } // Fin del método setNombre
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    } // Fin del método getApellidoPaterno
+
+    public void setApellidoPaterno(String pApellidoPaterno) {
+        apellidoPaterno = pApellidoPaterno;
+    } // Fin del método setApellidoPaterno
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    } // Fin del método getApellidoMaterno
+
+    public void setApellidoMaterno(String pApellidoMaterno) {
+        apellidoMaterno = pApellidoMaterno;
+    } // Fin del método setApellidoMaterno
+
+    public void imprimirCampos() {
+        System.out.println(
+            "Persona:" +
+            "\n\tnombre = " + nombre +
+            "\n\tapellidoPaterno = " + apellidoPaterno +
+            "\n\tapellidoMaterno = " + apellidoMaterno
+        );
+    } // Fin del método imprimirCampos
 
 } // Fin de la clase Persona
-
-class Acciones {
-
-    public static void imprimeCambioDeEdad(Persona persona) {
-        persona.edad = persona.edad + 10;
-        System.out.println("Edad: " + persona.edad);
-    } // Fin del método imprimeCambioDeEdad
-
-} // Fin de la clase Acciones
 
 public class Principal {
 
     public static void main(String[] args) {
 
-        Persona objeto1 = new Persona();
+        Persona persona1 = new Persona();
 
-        objeto1.edad = 15;
+        persona1.setNombre("Alberto");
+        persona1.setApellidoPaterno("Arenas");
+        persona1.setApellidoMaterno("Aguirre");
 
-        System.out.println("Edad (antes): " + objeto1.edad);
-
-        Acciones.imprimeCambioDeEdad(objeto1);
-
-        System.out.println("Edad (después): " + objeto1.edad);
+        persona1.imprimirCampos();
 
     } // Fin del método main
 
