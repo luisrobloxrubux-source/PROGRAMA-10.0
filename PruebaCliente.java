9f20b80011a45ecccbe0d3b20bbb98990bd34f68
luisrobloxrubux-source <luisrobloxrubux@gmail.com>
2026-07-17 18:06:50 -0500

    Add files via upload

--- PruebaCliente.java
+++ PruebaCliente.java
@@ -1,60 +1,70 @@
 
 
-class Cliente {
+class Persona {
 
     private String nombre;
     private String apellidoPaterno;
     private String apellidoMaterno;
-    private int numeroDeCliente;
 
-    public Cliente() {
+    public Persona() {
         nombre = "";
         apellidoPaterno = "";
         apellidoMaterno = "";
-        numeroDeCliente = 0;
-    } // Fin del constructor
+    }
 
     public String getNombre() {
         return nombre;
-    } // Fin del método getNombre
+    }
 
     public void setNombre(String pNombre) {
         nombre = pNombre;
-    } // Fin del método setNombre
+    }
 
     public String getApellidoPaterno() {
         return apellidoPaterno;
-    } // Fin del método getApellidoPaterno
+    }
 
     public void setApellidoPaterno(String pApellidoPaterno) {
         apellidoPaterno = pApellidoPaterno;
-    } // Fin del método setApellidoPaterno
+    }
 
     public String getApellidoMaterno() {
         return apellidoMaterno;
-    } // Fin del método getApellidoMaterno
+    }
 
     public void setApellidoMaterno(String pApellidoMaterno) {
         apellidoMaterno = pApellidoMaterno;
-    } // Fin del método setApellidoMaterno
+    }
+
+} // Fin de la clase Persona
+
+class Cliente extends Persona {
+
+    private int numeroDeCliente;
+
+    public Cliente() {
+        setNombre("");
+        setApellidoPaterno("");
+        setApellidoMaterno("");
+        numeroDeCliente = 0;
+    }
 
     public int getNumeroDeCliente() {
         return numeroDeCliente;
-    } // Fin del método getNumeroDeCliente
+    }
 
     public void setNumeroDeCliente(int pNumeroDeCliente) {
         numeroDeCliente = pNumeroDeCliente;
-    } // Fin del método setNumeroDeCliente
+    }
 
     public void imprimirCampos() {
         System.out.println(
-            "Cliente:" +
-            "\n\tnombre = " + nombre +
-            "\n\tapellidoPaterno = " + apellidoPaterno +
-            "\n\tapellidoMaterno = " + apellidoMaterno +
-            "\n\tnumeroDeCliente = " + numeroDeCliente
+            "Cliente: nombre = " + getNombre()
+            + " apellidoPaterno = " + getApellidoPaterno()
+            + " apellidoMaterno = " + getApellidoMaterno()
+            + " numeroDeCliente = " + numeroDeCliente
         );
-    } // Fin del método imprimirCampos
+    }
 
 } // Fin de la clase Cliente
 
@@ -64,13 +74,13 @@ public class PruebaCliente {
 
         Cliente cliente1 = new Cliente();
 
-        cliente1.setNombre("Alberto");
-        cliente1.setApellidoPaterno("Arenas");
-        cliente1.setApellidoMaterno("Aguirre");
-        cliente1.setNumeroDeCliente(1001);
+        cliente1.setNombre("Carlos");
+        cliente1.setApellidoPaterno("Cadena");
+        cliente1.setApellidoMaterno("Castillo");
+        cliente1.setNumeroDeCliente(2001);
 
         cliente1.imprimirCampos();
 
-    } // Fin del método main
+    }
 
-} // Fin de la clase PruebaCliente
+}
