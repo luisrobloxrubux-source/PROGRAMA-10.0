dabfc808c86765863c4f6b1d314514df09795562
luisrobloxrubux-source <luisrobloxrubux@gmail.com>
2026-07-18 20:48:58 -0500

    Add files via upload

--- PruebaPersona.java
+++ PruebaPersona.java
@@ -1,18 +1,44 @@
 
 
-class PruebaPersona {
+class Persona {
+    private String nombre;
+    private int edad;
 
+    public Persona() {
+        nombre = "(Sin nombre)";
+        edad = 0;
+    }
+
+    public void imprimirCampos() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String valor) {
+        nombre = valor;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int valor) {
+        edad = valor;
+    }
+}
+
+public class PruebaPersona {
     public static void main(String[] args) {
-
-        Persona persona = new Persona("Alex", 20);
-
+        Persona persona = new Persona();
         persona.imprimirCampos();
 
-        persona.setNombre("Beto");
-        persona.setEdad(19);
-
+        persona.setNombre("Alex");
+        persona.setEdad(20);
         persona.imprimirCampos();
-
-    } // Fin del método main
-
-} // Fin de la clase PruebaPersona
+    }
+}
